8b8d72691eee0aab7d331db03427bf9a2c7503be
JacobL95 <35790306+JacobL95@users.noreply.github.com>
2018-05-24 21:34:29 -0500

    Utility That Connects Web App To Server

---
+++ MD/LoginUtil.php
@@ -0,0 +1,26 @@
+<?php
+ DEFINE("LOCALHOST", "localhost");
+ DEFINE("USERNAME", "root");
+ DEFINE("PASSWORD", "");
+ DEFINE("DBNAME", "marriage_database");
+
+function user_login($UN, $PASS)
+{
+    $conn = new mysqli(LOCALHOST, USERNAME, PASSWORD, DBNAME);
+    //if ($conn->connection_error){
+      //  $conn->close();
+        //return "ConnectionFailed";
+    //}
+    
+    $sql = "SELECT User_Name FROM Login WHERE '" . $UN . "' = User_Name AND '" . $PASS."' = Password";
+    $result = $conn->query($sql);
+    
+    if($result->num_rows == 1){
+        $row = $result->fetch_assoc();
+        $conn->close();
+        return $row;
+    }
+    $conn->close();
+    return "LoginFailed";         
+}
+?>
