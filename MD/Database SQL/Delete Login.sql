ebcfebb322785d5c6d384634b3bdd2109d41cef3
JacobL95 <35790306+JacobL95@users.noreply.github.com>
2018-05-26 00:41:57 -0500

    Add files via upload

---
+++ MD/Database SQL/Delete Login.sql
@@ -0,0 +1,3 @@
+delete FROM marriage_database.login Where User_Name = 'Barbara';
+delete FROM marriage_database.login Where User_Name = 'Ricky';
+delete FROM marriage_database.login Where User_Name = 'Admin5';
