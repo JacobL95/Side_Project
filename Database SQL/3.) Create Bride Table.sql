cede4bb6bb1d0c5a97a6317afdc64a30de94757a
JacobL95 <35790306+JacobL95@users.noreply.github.com>
2018-06-03 15:05:02 -0500

    Delete 3.) Create Bride Table.sql

--- Database SQL/3.) Create Bride Table.sql
+++
@@ -1,9 +0,0 @@
-use Marriage_Database;
-
-Create table `Bride` (
-`Bride_ID` int auto_increment not null unique,
-`Bride_First_Name` varchar(50) not null,
-`Bride_Middle_Int` varchar(2) not null,
-`Bride_Last_Name` varchar (50) not null,
-primary key (`Bride_ID`)
-);
