c6f6d969c13089ea4e3fe39f0a732db69c9bbf88
JacobL95 <35790306+JacobL95@users.noreply.github.com>
2018-06-03 15:06:08 -0500

    Delete Delete Login.sql

--- Database SQL/Delete Login.sql
+++
@@ -1,3 +0,0 @@
-delete FROM marriage_database.login Where User_Name = 'Barbara';
-delete FROM marriage_database.login Where User_Name = 'Ricky';
-delete FROM marriage_database.login Where User_Name = 'Scott';
