00938dcb91a1b2387b923a4c366293d9be3d14d6
JacobL95 <35790306+JacobL95@users.noreply.github.com>
2018-06-03 15:05:20 -0500

    Delete 6.) Create Dispensation Type Table.sql

--- Database SQL/6.) Create Dispensation Type Table.sql
+++
@@ -1,7 +0,0 @@
-use Marriage_Database;
-
-Create table `Dispensation_Type` (
-`Dispensation_Type_Number` smallint auto_increment not null unique,
-`Dispensation_Type` varchar(23) not null,
-primary key (`Dispensation_Type_Number`)
-);
