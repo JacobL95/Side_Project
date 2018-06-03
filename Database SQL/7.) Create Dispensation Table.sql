c6698463dff9434a488d9a0f693fd13a58090a2d
JacobL95 <35790306+JacobL95@users.noreply.github.com>
2018-06-03 15:05:27 -0500

    Delete 7.) Create Dispensation Table.sql

--- Database SQL/7.) Create Dispensation Table.sql
+++
@@ -1,7 +0,0 @@
-use Marriage_Database;
-
-Create table `Dispensation` (
-`Dispensation_ID` smallint auto_increment not null unique,
-`Dispensation_Category` varchar(20) not null,
-primary key (`Dispensation_Category`)
-);
